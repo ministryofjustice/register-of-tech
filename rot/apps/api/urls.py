5d52959110a36f93331d88ba6a2ed128e2d8cf65
Josh Rowe <josh@s-block.com>
2017-05-15 17:11:14 +0100

    Add People and BusinessArea endpoints to API

--- rot/apps/api/urls.py
+++ rot/apps/api/urls.py
@@ -4,13 +4,16 @@ from django.conf.urls import url, include
 from rest_framework import routers
 from rest_framework_swagger.views import get_swagger_view
 
-from api.views import CategoryViewSet, ItemViewSet
+from api.views import (
+    CategoryViewSet, ItemViewSet, BusinessAreaViewSet, PeopleViewSet)
 
 schema_view = get_swagger_view(title='Register of Things')
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'categories', CategoryViewSet)
 router.register(r'items', ItemViewSet)
+router.register(r'business_area', BusinessAreaViewSet)
+router.register(r'people', PeopleViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
