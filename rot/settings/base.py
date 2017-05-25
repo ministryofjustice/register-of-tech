16799cc14ea5ca50a8cf875d3f8408eb08074f93
Josh Rowe <josh@s-block.com>
2017-05-25 12:06:22 +0100

    Get allowed hosts from env var for depolys to aws

--- rot/settings/base.py
+++ rot/settings/base.py
@@ -36,8 +36,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'CHANGE_ME')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    os.environ.get('ALLOWED_HOST', '*')
+]
 
 # Application definition
 
