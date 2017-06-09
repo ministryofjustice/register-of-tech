f13c7ada17397e58df6f863882a9bc53c4ded1f0
Josh Rowe <josh@s-block.com>
2017-06-09 11:37:03 +0100

    Update default permission class for application to allow read only for anonymous users and apply django model permissions for the rest

--- rot/settings/base.py
+++ rot/settings/base.py
@@ -173,7 +173,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
