b1ae3a5b19417658ccbe46e26c6f08f202b6f6e0
Josh Rowe <josh@s-block.com>
2017-06-16 12:08:08 +0100

    add application and superuser secrets on build

--- docker/run.sh
+++ docker/run.sh
@@ -6,7 +6,10 @@ set -e
 /usr/bin/python3 manage.py  migrate --run-syncdb --settings rot.settings.base --noinput
 
 # Load fixtures
-/usr/bin/python3 manage.py loaddata test_categories test_business_area groups
+/usr/bin/python3 manage.py loaddata test_categories test_business_area groups applications
+
+# Set secrets on Person and Application
+/usr/bin/python3 manage.py set_secrets
 
 # Run server
 /usr/bin/uwsgi --http-socket :$1 --plugin python --ini /app/conf/uwsgi.ini
