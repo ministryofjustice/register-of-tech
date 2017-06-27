d4d885918e07887868bc323fb393b7620332afc8
Josh Rowe <josh@s-block.com>
2017-06-27 10:52:32 +0100

    fix anonymous django guardian permissions  - Previously was erroring when trying to check write permissions for anonymous user

--- rot/apps/api/permissions.py
+++ rot/apps/api/permissions.py
@@ -6,5 +6,9 @@ class DjangoObjectPermissionsAnonReadOnly(DjangoObjectPermissions):
     authenticated_users_only = False
 
     def has_permission(self, request, view):
+        if request.method not in ['GET', 'HEAD', 'OPTIONS'] and \
+                not request.user.is_authenticated():
+            return False
+        
         return super().has_permission(request, view) or \
                self.has_object_permission(request, view, view.get_object())
