2275f8897588c08c136999cef085d2dc3cb504c1
Josh Rowe <josh@s-block.com>
2017-07-26 21:03:52 +0100

    add initial frontend app and templates

---
+++ rot/apps/frontend/forms.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+from django import forms
+
+from person.models import Person
+from register.models import Category, BusinessArea
+
+
+class AddItemForm1(forms.Form):
+    """
+    Form for adding categories to an Item
+    First form to be used in AddItemWizard view from views
+    see https://github.com/django/django-formtools/
+    """
+    categories = forms.ModelChoiceField(
+        required=True,
+        widget=forms.CheckboxSelectMultiple,
+        queryset=Category.objects.all(),
+    )
+
+
+
+class AddItemForm2(forms.Form):
+    """
+    Form for adding areas to an Item
+    """
+    areas = forms.ModelChoiceField(
+        required=True,
+        widget=forms.CheckboxSelectMultiple,
+        queryset=BusinessArea.objects.all(),
+    )
+
+
+class AddItemForm3(forms.Form):
+    """
+    Form for addin name and description to an item
+    """
+    name = forms.CharField(max_length=200)
+    description = forms.CharField(widget=forms.Textarea)
+
+
+class AddItemForm4(forms.Form):
+    """
+    Form for adding an owner to an Item
+    """
+    # TODO - this will need some way of creating a Person if that person
+    # doesn't exist in the DB
+    owner = forms.ModelChoiceField(
+        required=True,
+        queryset=Person.objects.all(),
+    )
+
+
+class SearchForm(forms.Form):
+    """Search Form"""
+    search = forms.CharField(max_length=100)
